5558cae2151bd0630c7d8f4738614453afa7b4d0
liwenhan220 <47604011+liwenhan220@users.noreply.github.com>
2023-06-26 22:06:34 -0400

    Add files via upload

---
+++ generate_data.py
@@ -0,0 +1,81 @@
+from extract_feature import *
+import os
+
+
+directories = ["extracted_data", "debug"]
+
+for dir in directories:
+    if not os.path.isdir(dir):
+        os.mkdir(dir)
+
+raw_folder = "raw_data/"
+
+ecoli = "Ecoli-positive/"
+styphi = "Styphi-positive/"
+negative = "negative/"
+
+test_bacts = []
+train_bacts = []
+max_shape = (1, 1)
+test_split_index = 1
+
+# load ecoli data
+for i in range(1, 11):
+    name = "E.coli + {}.bmp".format(i)
+    input_img_path = raw_folder + ecoli + name
+    img = cv2.imread(input_img_path)
+    bacts, shape = generate_bacts(img, 0, debug = True, debug_path = directories[1] + '/' + name, image_name=name, threshold = 0.7, max_diameter=15)
+    if i <= test_split_index:
+        test_bacts += bacts
+    else:
+        train_bacts += bacts
+    max_shape = max_box(max_shape, shape)
+
+# load styphi data
+for i in range(1, 17):
+    name = "S.typhi + {}.bmp".format(i)
+    input_img_path = raw_folder + styphi + name
+    img = cv2.imread(input_img_path)
+    bacts, shape = generate_bacts(img, 0, debug = True, debug_path = directories[1] + '/' + name, image_name=name, threshold = 0.0,  max_diameter=10)
+    if i <= test_split_index:
+        test_bacts += bacts
+    else:
+        train_bacts += bacts
+    max_shape = max_box(max_shape, shape)
+
+# load negative data
+for i in range(1, 12):
+    name = "swab-{}.bmp".format(i)
+    input_img_path = raw_folder + negative + "swab-{}.bmp".format(i)
+    img = cv2.imread(input_img_path)
+    if i > 1:
+        bacts, shape = generate_bacts(img, 1, cover_corners= False, debug = True, debug_path = directories[1] + '/' + name, image_name=name, threshold = 0, max_diameter=float('inf'))
+    else:
+        bacts, shape = generate_bacts(img, 1, cover_corners= False, debug = True, debug_path = directories[1] + '/' + name, image_name=name, threshold = 0, max_diameter=float('inf'))
+
+    if i <= test_split_index:
+        test_bacts += bacts
+    else:
+        train_bacts += bacts
+    max_shape = max_box(max_shape, shape)
+
+X = []
+y = []
+for bact in train_bacts:
+    bact.pad_img(max_shape)
+    X.append(bact.bg_normalized())
+    y.append(bact.label)
+
+np.save(directories[0] + "/trainX.npy", X)
+np.save(directories[0] + "/trainY.npy", y)
+
+testX = []
+testY = []
+
+for bact in test_bacts:
+    bact.pad_img(max_shape)
+    testX.append(bact.bg_normalized())
+    testY.append(bact.label)
+
+np.save(directories[0] + "/testX.npy", testX)
+np.save(directories[0] + "/testY.npy", testY)
---
+++ label_img.py
@@ -0,0 +1,80 @@
+from extract_feature import *
+from cnn import *
+import torch
+import numpy as np
+import cv2
+import sys
+import getopt
+import pdb
+
+argv = sys.argv[1:]
+
+try:
+    opts, args = getopt.getopt(argv, "m:i:o:", 
+                                ["model_path=",
+                                "image_path=",
+                                "output_path="])
+except:
+    print("Error")
+
+MODEL_PATH = 'models/bact_model_49'
+IMG_NAME = "raw_data/negative/swab-1.bmp"
+OUTPUT_PATH = "labeled.bmp"
+
+
+# IMG_NAME = "raw_data/Ecoli-positive/E.coli + 10.bmp"
+# IMG_NAME = "raw_data/Styphi-positive/S.typhi + 1.bmp"
+# IMG_NAME = "raw_data/negative/swab-1.bmp"
+
+
+for opt, arg in opts:
+    if opt in ['-m', '--model_path']:
+        MODEL_PATH = arg
+
+    elif opt in ['-i', '--image_path']:
+        IMG_NAME = arg
+
+    elif opt in ['-o', '--output_path']:
+        OUTPUT_PATH = arg
+
+
+print('model name: {}'.format(MODEL_PATH))
+print('img name: {}'.format(IMG_NAME))
+print('output path: {}'.format(OUTPUT_PATH))
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+model = LeNet5(2)
+model.load_state_dict(torch.load(MODEL_PATH))
+model.to(device=device)
+model.eval()
+max_shape = np.load('extracted_data/testX.npy')[0].shape[:2]
+
+img = cv2.imread(IMG_NAME)
+bacts, _ = generate_bacts(img, 0, cover_corners= False, debug = False, debug_path = None, threshold=0, max_diameter=float('inf'))
+
+
+bact_count = 0
+noise_count = 0
+pbar = tqdm(bacts, total=len(bacts))
+for bact in pbar:
+
+    if bact.pad_img(max_shape) == -1:
+        continue
+    f_img = bact.bg_normalized()
+    tensor_img = torch.tensor(reshape_data(f_img.reshape(1, *f_img.shape))).to(device)
+    output = model(tensor_img)[0]
+    pbar.set_description('drawing bacteria on image')
+    label = torch.argmax(output)
+    if label == 0:
+        color = [0, 255, 0]
+        bact_count += 1
+    else:
+        color = [255, 0, 0]
+        noise_count += 1
+    draw_bacteria(img, bact, color)
+
+print('bacteria count: {}\nnoise count: {}\nbact rate: {}'.format(bact_count, noise_count, bact_count / (bact_count + noise_count)))
+cv2.imwrite('labeled_img.bmp', img)
+
